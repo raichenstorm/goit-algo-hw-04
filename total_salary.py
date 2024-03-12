9b52a39c0ebeaca2d533ade4ba5cd599b4d3d7d9
raichenstorm <raichenstorm@gmail.com>
2024-03-12 12:03:07 +0200

    1

---
+++ total_salary.py
@@ -0,0 +1,25 @@
+def total_salary(path):
+    salary = 0
+    employees_number = 0
+    total = 0
+
+    try:
+        with open(path, 'r') as fh:
+            for line in fh:
+                line_prepared = line.strip()
+                name, salary = line_prepared.split(',')
+                total += int(salary)
+                employees_number += 1
+        if employees_number == 0:
+            return 0, 0
+        average_salary = int(total / employees_number)
+        return total, average_salary
+    except FileNotFoundError:
+        print(f"Файл {path} не знайдено.")
+        return 0, 0
+    except Exception:
+        print('Виникла помилка.')
+        return 0, 0
+
+total, average = total_salary(r"C:\Users\Admin\Documents\GitHub\goit-algo-hw-04\salaries.txt")
+print(f'Загальна заробітня плата - {total}, середня заробітня плата - {average}.')
